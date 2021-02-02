aac193a84be91b30f5a848a7a4b89ebf11428239
James Hoekzema <james.hoekzema@wwt.com>
2021-02-02 14:20:35 -0600

    save: add mockdate

---
+++ Dates/mockdate.test.js
@@ -0,0 +1,49 @@
+const assert = require('assert');
+const mockDate = require('mockdate');
+const schedule = require('./reminders');
+
+describe('get daily reminders', () => {
+    afterEach(() => {
+        mockDate.reset();
+        schedule.clearReminders();
+    });
+
+    it('should return a reminder set for a Monday', () => {
+        schedule.addDailyReminder({
+            label: 'test',
+            start: '2021-02-01',
+            end: '2021-02-01',
+            days: [schedule.day.monday],
+        });
+
+        mockDate.set(new Date('2021-01-25T12:00:00.000Z')); // Previous Monday
+        assert.deepStrictEqual(schedule.getDailyReminders(), []);
+
+        mockDate.set(new Date('2021-02-01T12:00:00.000Z')); // Monday
+        assert.deepStrictEqual(schedule.getDailyReminders(), ['test']);
+
+        mockDate.set(new Date('2021-02-08T12:00:00.000Z')); // Next Monday
+        assert.deepStrictEqual(schedule.getDailyReminders(), []);
+    });
+
+    it('should return a reminder set for a weekend', () => {
+        schedule.addDailyReminder({
+            label: 'test',
+            start: '2021-02-01',
+            end: '2021-02-10',
+            days: [schedule.day.saturday, schedule.day.sunday],
+        });
+
+        mockDate.set(new Date('2021-02-05T12:00:00.000Z')); // Friday
+        assert.deepStrictEqual(schedule.getDailyReminders(), []);
+
+        mockDate.set(new Date('2021-02-06T12:00:00.000Z')); // Saturday
+        assert.deepStrictEqual(schedule.getDailyReminders(), ['test']);
+
+        mockDate.set(new Date('2021-02-07T12:00:00.000Z')); // Sunday
+        assert.deepStrictEqual(schedule.getDailyReminders(), ['test']);
+
+        mockDate.set(new Date('2021-02-08T12:00:00.000Z')); // Monday
+        assert.deepStrictEqual(schedule.getDailyReminders(), []);
+    });
+});
